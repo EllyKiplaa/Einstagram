78c951cf500b427d0bb9bdb295ae4cff1d90dee6
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-09 20:05:43 +0300

    Import the views from django and created a view list

--- socioapp/views.py
+++ socioapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import(
+
+ listview)
+
 
 # Create your views here.
+
+class PostListView(ListView):
+    template_name = "socioapp/post_list.html"
+    queryset = post.objects.all()
+    context.object_name ='posts'
