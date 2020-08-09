8fa0e0eddfcccd87f8da33bf8fac7584165d1654
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-10 00:15:21 +0300

    add static root and url to the settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -119,5 +119,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# configuring the location for media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
