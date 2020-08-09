9868910508e8092ce1309d0b2992d0964bf7e90f
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-09 16:40:12 +0300

    add the post class in the models.py

--- socioapp/models.py
+++ socioapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey("auth.User",on_delete.CASCADE)
+    image = models.ImageField(blank=True, null=True)
+    caption = models.models.TextField()
+    create_date = model.DateTime(default=timezone.now)
