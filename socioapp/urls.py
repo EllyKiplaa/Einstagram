4103919833d8943896835ab18f631132634b3a9c
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-10 19:12:26 +0300

    Import from path

--- socioapp/urls.py
+++ socioapp/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
+from django.conf.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.views.static import serve
 from django.urls import path
 
 from .views import(
-    PostListView
+    PostListView,
+    PostCreateView,
 )
 
 app_name = "sociaapp"
@@ -13,9 +14,11 @@ app_name = "sociaapp"
 urlpatterns = [
     #local :
     path("", PostListView.as_view(), name='post_list'),
-    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path("/new", PostCreateView.as_view(), name='post_create'),
+    
+    # path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    # path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
