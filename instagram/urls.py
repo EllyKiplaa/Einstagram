6c7e084d39ca30c6cfd4b5e13b62488653c35c30
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-10 17:53:57 +0300

    Import path and make them be in the url pattens

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.contrib.auth import views 
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('socioapp.urls'))
-
-]
+    path(r'admin/', admin.site.urls),
+    path(r'', include('socioapp.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
