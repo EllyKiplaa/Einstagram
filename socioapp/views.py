c3865afe641140b636adc664c1327b2e5969293c
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-10 19:13:18 +0300

    add the CreateView

--- socioapp/views.py
+++ socioapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
+from .forms import PostForm
 from .models import Post
 from django.views.generic import(
+    ListView,
+    CreateView
 
- ListView
  )
 
 
@@ -12,3 +14,14 @@ class PostListView(ListView):
     template_name = "/home/elly/Core/Einstagram/socioapp/templates/post_list.html"
     queryset = Post.objects.all()
     context_object_name ='posts'
+
+class PostCreateView(CreateView):
+    template = '/home/elly/Core/Einstagram/socioapp/templates/post_create.html'
+    from_class = postFrom
+    querset = post.objects.all()
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        form.instance.auther = self.request.user
+
+        return super().form_valid(form)
