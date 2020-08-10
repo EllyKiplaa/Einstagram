f796faa1ec8e133fa4b21040dd40a3d03c168535
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-10 17:57:27 +0300

    socioapp/urls.py

--- socioapp/urls.py
+++ socioapp/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.urls import path
 
 from .views import(
     PostListView
@@ -11,9 +12,9 @@ app_name = "sociaapp"
 
 urlpatterns = [
     #local :
-    url("", PostListView.as_view(), name='post_list'),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path("", PostListView.as_view(), name='post_list'),
+    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
