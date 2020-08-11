b4f7f0e7cef58fddf10d1da05be7af0f5c638abf
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-11 19:02:18 +0300

    add commit remove the highlighted codes

--- socioapp/urls.py
+++ socioapp/urls.py
@@ -1,8 +1,5 @@
-from django.conf.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.views.static import serve
-from django.urls import path
+from django.conf.urls import url,include
+from . import views
 
 from .views import(
     PostListView,
@@ -13,12 +10,8 @@ app_name = "sociaapp"
 
 urlpatterns = [
     #local :
-    path("", PostListView.as_view(), name='post_list'),
-    path("/new", PostCreateView.as_view(), name='post_create'),
+    url(r"^$", PostListView.as_view(), name='post_list'),
+    url(r"^new/", PostCreateView.as_view(), name='post_create'),
     
-    # path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    # path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+   
 ]
-
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
