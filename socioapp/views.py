3e2ea0768c678d49b0237fb724a89b1058375d6d
EllyKiplaa <ellykiplangat200@gmail.com>
2020-08-11 19:03:00 +0300

    add the detailview

--- socioapp/views.py
+++ socioapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect
 from .forms import PostForm
 from .models import Post
+from django.utils import timezone 
 from django.views.generic import(
     ListView,
-    CreateView
+    CreateView,
+    DetailView
 
  )
 
@@ -16,12 +18,16 @@ class PostListView(ListView):
     context_object_name ='posts'
 
 class PostCreateView(CreateView):
-    template = '/home/elly/Core/Einstagram/socioapp/templates/post_create.html'
-    from_class = postFrom
-    querset = post.objects.all()
+    template_name = '/home/elly/Core/Einstagram/socioapp/templates/post_create.html'
+    form_class = PostForm
+    querset = Post.objects.all()
+    success_url ='/'
 
     def form_valid(self, form):
         print(form.cleaned_data)
-        form.instance.auther = self.request.user
-
+        form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostDetailView(DetailView):
+
+    queryset = Post.objects.all()
